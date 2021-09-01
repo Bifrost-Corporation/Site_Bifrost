2027425e0eeaa6383a73b292e1930341e3e5e0e2
Douglas Nascimento <douglas.dans@gmail.com>
2021-09-01 11:25:53 -0300

    VM PWA FUNCIONA

--- service-worker.js
+++ service-worker.js
@@ -1,71 +1,58 @@
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(
-        [
-          'index.html',
-          'css/style.css',
-          'js/mobile-navbar.js'
-        ]
-      );
+var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
+var VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
+var CACHE_NAME = APP_PREFIX + VERSION
+var URLS = [                            // Add URL you want to cache in this list.
+  '/Site_Bifrost/',                     // If you have separate JS/CSS files,
+  '/Site_Bifrost/index.html'            // add path to those files here
+]
+
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
+
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
     })
-  );
-});
+  )
+})
 
-self.addEventListener('fetch', event => {
-  // Cache-First Strategy
-  event.respondWith(
-    caches.match(event.request) // check if the request has already been cached
-    .then(cached => cached || fetch(event.request)) // otherwise request network
-  );
-});
+// Cache resources
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(URLS)
+    })
+  )
+})
 
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      // `keyList` contains all cache names under your username.github.io
+      // filter out ones that has this app prefix to create white list
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      // add current cache name to white list
+      cacheWhitelist.push(CACHE_NAME)
 
-
-
-
-
-
-
-
-function registerBackgroundSync() {
-  if (!navigator.serviceWorker){
-      return console.error("Service Worker not supported")
-  }
-
-  navigator.serviceWorker.ready
-  .then(registration => registration.sync.register('syncAttendees'))
-  .then(() => console.log("Registered background sync"))
-  .catch(err => console.error("Error registering background sync", err))
-}
-
-
-
-
-
-
-
-
-
-self.addEventListener('sync', function(event) {
-	console.log("sync event", event);
-    if (event.tag === 'syncAttendees') {
-        event.waitUntil(syncAttendees()); // sending sync request
-    }
-});
-
-
-
-
-
-
-
-
-
-function syncAttendees(){
-	return update({ url: `https://reqres.in/api/users` })
-    	.then(refresh)
-    	.then((attendees) => self.registration.showNotification(
-    		`${attendees.length} attendees to the PWA Workshop`
-    	))
-}
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          console.log('deleting cache : ' + keyList[i] )
+          return caches.delete(keyList[i])
+        }
+      }))
+    })
+  )
+})
